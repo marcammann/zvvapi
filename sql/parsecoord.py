7c28bd9b71dd85f972951bd1c207b2e73d19a1a9
Marc Ammann <marc@codesofa.com>
2009-03-23 17:13:16 +0100

    some new stations

--- sql/parsecoord.py
+++ sql/parsecoord.py
@@ -2,40 +2,65 @@
 # -*- coding: UTF-8 -*-
 
 import csv
+import sys
 
-f = open('./coord.csv')
-reader =  csv.reader(f, delimiter=',', quotechar='"')
+filename = sys.argv[1]
+creator = sys.argv[2]
 
-out = open('./geodata.sql', 'w')
+convlon = lambda xx, yy: (2.6779094 + 4.728982 * yy + 0.791484 * xx * yy + 0.1306 * yy * (xx * xx) - 0.0436 * (yy * yy * yy)) * 100 / 36
+convlat = lambda xx, yy: (16.9023892 + 3.238272 * xx - 0.270978 * (yy * yy) - 0.002528 * (xx * xx) - 0.0447 * (yy * yy) * xx - 0.0140 * (xx * xx * xx)) * 100 / 36
 
-for line in reader:
-    id = line[0]
-    y = float(line[1])
-    x = float(line[2])
-    name = line[3]
-    yy = (y-600000)/1000000
-    xx = (x-200000)/1000000
+if (creator == 'sbb'):
+	f = open(filename)
+	reader =  csv.reader(f, delimiter=',', quotechar='"')
 
-    lonT = (2.6779094
-            + 4.728982 * yy 
-            + 0.791484 * xx * yy
-            + 0.1306 * yy * (xx * xx)
-            - 0.0436 * (yy * yy * yy))
-    lon = lonT * 100 / 36
+	out = open('./geodata-sbb.sql', 'w')
 
-    latT = (16.9023892
-            + 3.238272 * xx
-            - 0.270978 * (yy * yy)
-            - 0.002528 * (xx * xx)
-            - 0.0447 * (yy * yy) * xx
-            - 0.0140 * (xx * xx * xx))
-    lat = latT * 100 / 36    
+	for line in reader:
+		id = line[0]
+		y = float(line[1])
+		x = float(line[2])
+		name = line[3]
+		yy = (y-600000)/1000000
+		xx = (x-200000)/1000000
+	
+		lon = convlon(xx, yy)
+		lat = convlat(xx, yy)
 
-    lat = '%.6f' % lat
-    lon = '%.6f' % lon
+		lat = '%.6f' % lat
+		lon = '%.6f' % lon
 
-    out.write('INSERT INTO zvv_station (station_sbbid, station_lat, station_lon, station_name) VALUES (' + id + ', ' + lat + ', ' + lon + ', "' + name + '");\n')
+		out.write('INSERT INTO zvv_station (station_sbbid, station_lat, station_lon, station_name) VALUES (' + id + ', ' + lat + ', ' + lon + ', "' + name + '");\n')
 
-out.close()
-    
+	out.close()
+elif (creator == 'zvv'):
+	f = open(filename)
+	reader =  csv.reader(f, delimiter=',', quotechar='"')
 
+	out = open('./geodata-zvv.sql', 'w')
+
+	for line in reader:
+		try:
+			zvvid = line[0]
+			sbbid = line[2]
+			y = float(line[3])
+			x = float(line[4])
+		except ValueError:
+			continue
+			
+		if sbbid == 0:
+			continue
+			
+		name = line[1]
+		yy = (y-600000)/1000000
+		xx = (x-200000)/1000000
+	
+		lon = convlon(xx, yy)
+		lat = convlat(xx, yy)
+
+		lat = '%.6f' % lat
+		lon = '%.6f' % lon
+
+		out.write('INSERT INTO zvv_station (station_zvvid, station_sbbid, station_lat, station_lon, station_name) VALUES (' + zvvid + ',' + sbbid + ', ' + lat + ', ' + lon + ', "' + name + '");\n')
+
+	out.close()
--- sql/setup.sql
+++ sql/setup.sql
@@ -5,20 +5,23 @@ CREATE TABLE `zvv_apikey` (
     apikey_domain VARCHAR(255) NOT NULL DEFAULT '',
 
     PRIMARY KEY (apikey_id)
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 DROP TABLE IF EXISTS zvv_station;
 CREATE TABLE zvv_station (
     station_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     station_sbbid INT UNSIGNED NOT NULL DEFAULT 0,
+    station_zvvid INT UNSIGNED NOT NULL DEFAULT 0,    
     station_lat DECIMAL(9,6) NOT NULL DEFAULT 0.000000,
     station_lon DECIMAL(9,6) NOT NULL DEFAULT 0.000000,
     station_name VARCHAR(255) NOT NULL DEFAULT '',
     station_type VARCHAR(128) NOT NULL DEFAULT '',
     
     PRIMARY KEY (station_id),
-    UNIQUE(station_sbbid),
+    INDEX(station_sbbid),
+    INDEX(station_zvvid),
+    INDEX(station_name),    
     INDEX(station_lat),
     INDEX(station_lon)
-) CHARACTER SET utf8 COLLATE utf8_general_ci;
+) ENGINE=MyISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
 
